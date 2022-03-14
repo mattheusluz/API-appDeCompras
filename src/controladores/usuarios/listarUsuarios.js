594caffc99bfa0e4ae43679425e429354ac94697
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2022-03-14 15:50:02 -0300

    Implementada listagem de usuarios

---
+++ src/controladores/usuarios/listarUsuarios.js
@@ -0,0 +1,24 @@
+const knex = require('../../conexao');
+
+const listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await knex('usuarios');
+
+    if (!usuarios) {
+      return res.status(400).json({ erro: 'não foi possivel listar os usuarios.' });
+    }
+
+    const listaUsuarios = [];
+
+    for (const usuario of usuarios) {
+      const { senha, ...dadosUsuarios } = usuario
+      listaUsuarios.push(dadosUsuarios)
+    }
+
+    return res.status(200).json(listaUsuarios);
+  } catch (error) {
+    return res.status(400).json({ erro: error.message });
+  }
+}
+
+module.exports = listarUsuarios;
--- src/rotas.js
+++ src/rotas.js
@@ -2,11 +2,13 @@ const express = require('express');
 const login = require('./controladores/login');
 const atualizarUsuario = require('./controladores/usuarios/atualizarUsuario');
 const cadastrarUsuario = require('./controladores/usuarios/cadastrarUsuarios');
+const listarUsuarios = require('./controladores/usuarios/listarUsuarios');
 const verificaLogin = require('./filtros/verificaLogin');
 
 const rotas = express();
 
 rotas.post('/usuarios', cadastrarUsuario);
+rotas.get('/usuarios', listarUsuarios);
 rotas.post('/login', login);
 
 rotas.use(verificaLogin)
