9f2e883dd6249efde17cfac1994d8cbd5ee4fc2a
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2022-03-14 10:33:43 -0300

    Criada rota para cadastrar usuarios

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cadastrarUsuario = require('./controladores/usuarios/cadastrarUsuarios');
 
 const rotas = express;
+
+rotas.post('/usuarios', cadastrarUsuario);
 
 module.exports = rotas;
